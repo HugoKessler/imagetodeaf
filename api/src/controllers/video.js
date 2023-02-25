a8c9087b745788109e7f5b69df8b7dbe3879aed1
HugoKessler <kessler.hugo99@gmail.com>
2023-02-25 15:28:13 +0100

    route api to python api

--- api/src/controllers/video.js
+++ api/src/controllers/video.js
@@ -1,32 +1,28 @@
 const express = require("express");
 
 const router = express.Router();
-const fs = require("fs");
-const { promisify } = require("util");
-const exec = promisify(require("child_process").exec);
+const fetch = require("node-fetch");
 
+const pythonApiUrl = "http://localhost:5000";
 router.post("/file", async (req, res) => {
   try {
-    console.log("you are here");
-    console.log(req.body);
     const images = req.body;
-    //send images to python api
-    res.status(200).send({ ok: true });
+    const token = 50;
 
-    // res.json(images);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Internal Server Error");
-  }
-});
+    const summaryString = await fetch(pythonApiUrl + "/summary/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ images, token }),
+    });
 
-router.post("/image", async (req, res) => {
-  try {
-    console.log("you are here");
-    console.log(req.body);
-    const images = req.body;
+    const summaryAudio = await fetch(pythonApiUrl + "/audio/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(summaryString),
+    });
+
     //send images to python api
-    res.status(200).send({ ok: true });
+    res.status(200).send({ ok: true, data: summaryAudio });
 
     // res.json(images);
   } catch (error) {
@@ -39,9 +35,46 @@ router.post("/live", async (req, res) => {
   try {
     console.log("you are here");
     console.log(req.body);
-    const images = req.body;
+    const image = req.body;
+
+    const imageDescription = await fetch(pythonApiUrl + "/text/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(image),
+    });
+
+    const audioDescription = await fetch(pythonApiUrl + "/audio/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(imageDescription),
+    });
     //send images to python api
-    res.status(200).send({ ok: true });
+    res.status(200).send({ ok: true, data: summaryAudio });
+
+    // res.json(images);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+router.post("/image", async (req, res) => {
+  try {
+    const image = req.body;
+    //send images to python api
+
+    const imageDescription = await fetch(pythonApiUrl + "/text/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(image),
+    });
+
+    const audioDescription = await fetch(pythonApiUrl + "/audio/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(imageDescription),
+    });
+    res.status(200).send({ ok: true, data: audioDescription });
 
     // res.json(images);
   } catch (error) {
