fe7924ff4dbae7a9e14af9566833f7ca079cddd1
HugoKessler <kessler.hugo99@gmail.com>
2023-02-25 12:32:11 +0100

    up

--- admin/src/scenes/user/index.js
+++ admin/src/scenes/user/index.js
@@ -1,8 +1,4 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
-
-import User from "./list";
-import UserView from "./view";
 
 export default () => {
   return <div>You are here</div>;
