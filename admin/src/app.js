9b7d0265534ea55a5644fe76464ad66f740ac77e
HugoKessler <kessler.hugo99@gmail.com>
2023-02-25 12:34:08 +0100

    up

--- admin/src/app.js
+++ admin/src/app.js
@@ -2,16 +2,8 @@ import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import * as Sentry from "@sentry/browser";
 
-import { setUser } from "./redux/auth/actions";
-
-import Account from "./scenes/account";
-import Auth from "./scenes/auth";
 import User from "./scenes/user";
-import Drawer from "./components/drawer";
-
-import Header from "./components/header";
 
 import api from "./services/api";
 
